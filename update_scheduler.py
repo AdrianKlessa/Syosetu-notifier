bfa2f0589a1859d44a07337e1c2c26a9e160393b
Adrian Klessa <50918271+AdrianKlessa@users.noreply.github.com>
2024-04-15 21:38:47 +0200

    Scheduler framework definition

---
+++ update_scheduler.py
@@ -0,0 +1,16 @@
+# TODO: Implement config file
+def get_update_schedule():
+    pass
+
+
+def get_followed_novels():
+    # TODO: For each from followed novels use api_client
+    pass
+
+def get_update():
+    # TODO: Sanity check using the data in history file and current time here
+    pass
+
+def main_update_loop():
+    # TODO: In a while loop get the followed novels from file, get updates and then wait the configured amount of time
+    pass
