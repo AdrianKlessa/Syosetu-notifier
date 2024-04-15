243f561b03a10ee8b20fc9f22cb086c876309576
Adrian Klessa <50918271+AdrianKlessa@users.noreply.github.com>
2024-04-15 21:22:33 +0200

    Note for timeout handling

--- api_client.py
+++ api_client.py
@@ -1,17 +1,18 @@
 import requests
 import yaml
+
 main_url = "https://api.syosetu.com/novelapi/api/"
 
 mitsuba_monogatari_id = "n0388ee"
 
-test_id1="n0001a"
-test_id2="n1111b"
-test_id3="n9999d"
+test_id1 = "n0001a"
+test_id2 = "n1111b"
+test_id3 = "n9999d"
 
 print("-".join([test_id1]))
 
-print("-".join([test_id1,test_id2]))
-print("-".join([test_id1,test_id2,test_id3]))
+print("-".join([test_id1, test_id2]))
+print("-".join([test_id1, test_id2, test_id3]))
 
 
 # Response content for one example request (one novel) info was 573KB
@@ -21,32 +22,35 @@ def get_novels_info(novel_id_list):
     :param novel_id_list: List of strings defining the novel ncodes
     :return: yaml-formatted text and boolean indicating if the request was successful
     """
-    novels_string = "-".join(novel_id_list) # Novel ids are joined by "-" in the api (e.g. /api/?ncode=n0001a-n1111b-n9999d)
+    novels_string = "-".join(
+        novel_id_list)  # Novel ids are joined by "-" in the api (e.g. /api/?ncode=n0001a-n1111b-n9999d)
     response = requests.get(main_url, params={"ncode": novels_string})
-    if response.status_code==200:
+    if response.status_code == 200:
         response_data = response.text
         success = True
     else:
         response_data = None
         success = False
-    return (response_data,success)
+    return (response_data, success)
+
 
 def get_novels_info_dict(novel_id_list):
+    # TODO: Correct the error handling for timeouts etc. https://stackoverflow.com/questions/16511337/correct-way-to-try-except-using-python-requests-module
     """
 
     :param novel_id_list: List of strings defining the novel ncodes
     :return: python dictionary with novel information and boolean indicating if the request was successful
     """
-    novels_string = "-".join(novel_id_list) # Novel ids are joined by "-" in the api (e.g. /api/?ncode=n0001a-n1111b-n9999d)
+    novels_string = "-".join(
+        novel_id_list)  # Novel ids are joined by "-" in the api (e.g. /api/?ncode=n0001a-n1111b-n9999d)
     response = requests.get(main_url, params={"ncode": novels_string})
-    if response.status_code==200:
+    if response.status_code == 200:
         python_dict_response = yaml.load(response.text, Loader=yaml.BaseLoader)
         success = True
     else:
         python_dict_response = None
         success = False
-    return (python_dict_response,success)
-
+    return (python_dict_response, success)
 
 
 print(get_novels_info_dict([mitsuba_monogatari_id]))
