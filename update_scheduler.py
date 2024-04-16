8b15a7086949d860fcd5c45e2b6f6b408ec58b42
Adrian Klessa <50918271+AdrianKlessa@users.noreply.github.com>
2024-04-16 22:51:15 +0200

    Add config file & parsing

--- update_scheduler.py
+++ update_scheduler.py
@@ -1,17 +1,28 @@
+import configparser
+
+
 # TODO: Implement config file
 # TODO: Add a "verbose" debug option to notify when getting updates even if no novel was updated
-def get_update_schedule():
-    pass
+
+def read_config():
+    config_file = configparser.ConfigParser()
+    config_file.read('config.ini')
+    config = dict()
+    config["sleep_time"] = int(config_file["Scheduler"]["SleepTime"])
+    config["verbose"] = config_file["Scheduler"].getboolean("Verbose")
+    config["followed_novels"] = config_file["Novels"]["FollowedNovels"].split(',')
+    return config
 
 
 def get_followed_novels():
     # TODO: For each from followed novels use api_client
     pass
 
-def get_update():
-    # TODO: Sanity check using the data in history file and current time here
-    pass
 
 def main_update_loop():
+    # TODO: Sanity check using the data in history file and current time here
     # TODO: In a while loop get the followed novels from file, get updates and then wait the configured amount of time
     pass
+
+
+print(read_config())
