9019569114bea5aad29476015d93a06bdd696f08
Adrian Klessa <50918271+AdrianKlessa@users.noreply.github.com>
2024-04-18 21:23:13 +0200

    Remove TODO that was done

--- api_client.py
+++ api_client.py
@@ -19,7 +19,6 @@ print("-".join([test_id1, test_id2, test_id3]))
 # Down to 88 KB when limiting response parameters
 
 def get_novels_info_dict(novel_id_list,limit_parameters=True):
-    # TODO: Correct the error handling for timeouts etc. https://stackoverflow.com/questions/16511337/correct-way-to-try-except-using-python-requests-module
     """
     :param limit_parameters: If True, limits request to only ask for the fields needed by the application
     :param novel_id_list: List of strings defining the novel ncodes
