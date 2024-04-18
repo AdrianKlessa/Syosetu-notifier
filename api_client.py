c16af6cf2d6f6f9e45c53234152adc5e343d7f2d
Adrian Klessa <50918271+AdrianKlessa@users.noreply.github.com>
2024-04-18 21:16:53 +0200

    API error handling

--- api_client.py
+++ api_client.py
@@ -1,5 +1,6 @@
 import requests
 import yaml
+import notification_pusher
 
 main_url = "https://api.syosetu.com/novelapi/api/"
 
@@ -32,14 +33,26 @@ def get_novels_info_dict(novel_id_list,limit_parameters=True):
         # title, ncode, writer, general_firstup, general_lastup, novelupdated_at
         of_parameter = "-".join(["t", "n", "w", "gf", "gl", "nu"])
         request_params["of"]=of_parameter
-
-    response = requests.get(main_url, params=request_params)
-    if response.status_code == 200:
-        python_dict_response = yaml.load(response.text, Loader=yaml.BaseLoader)
-        success = True
-    else:
+    try:
+        response = requests.get(main_url, params=request_params)
+        if response.status_code == 200:
+            python_dict_response = yaml.load(response.text, Loader=yaml.BaseLoader)
+            success = True
+        else:
+            python_dict_response = None
+            success = False
+    except requests.exceptions.Timeout as errt:
         python_dict_response = None
         success = False
+        notification_pusher.custom_error_notification("Connection timeout while getting novel info!", repr(errt))
+    except requests.exceptions.TooManyRedirects as errtmr:
+        python_dict_response = None
+        success = False
+        notification_pusher.custom_error_notification("Too Many Redirects while getting novel info!", repr(errtmr))
+    except requests.exceptions.RequestException as e:
+        python_dict_response = None
+        success = False
+        notification_pusher.custom_error_notification("Error encountered while getting novel info!", repr(e))
     return (python_dict_response, success)
 
 
--- notification_pusher.py
+++ notification_pusher.py
@@ -1,10 +1,10 @@
 from winotify import Notification, audio
 import os
 
-
+app_id = "Syosetu notifier"
 # TODO: Pass novel information (name, title) to notifications
 def new_chapter_notification():
-    toast = Notification(app_id="Syosetu notifier",
+    toast = Notification(app_id=app_id,
                          title="New chapter for novel!",
                          msg="New chapter for novel!",
                          icon=os.path.abspath("res/bookmark-book.png"))
@@ -13,7 +13,7 @@ def new_chapter_notification():
 
 
 def novel_modified_notification():
-    toast = Notification(app_id="Syosetu notifier",
+    toast = Notification(app_id=app_id,
                          title="Novel chapter modified!",
                          msg="Novel chapter modified!",
                          icon=os.path.abspath("res/bookmark-book.png"))
@@ -22,10 +22,17 @@ def novel_modified_notification():
 
 
 def history_file_not_found_notification():
-    toast = Notification(app_id="Syosetu notifier",
+    toast = Notification(app_id=app_id,
                          title="History file missing!",
                          msg="History file could not be found despite previously being defined",
                          icon=os.path.abspath("res/bookmark-book.png"))
     toast.set_audio(audio.Default, loop=False)
+    toast.show()
 
+def custom_error_notification(title, msg):
+    toast = Notification(app_id=app_id,
+                         title=title,
+                         msg=msg,
+                         icon=os.path.abspath("res/bookmark-book.png"))
+    toast.set_audio(audio.Default, loop=False)
     toast.show()
